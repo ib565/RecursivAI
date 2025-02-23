5d63b791dea84e08d6bf95dbb34491444eccbc86
ib565 <ish.bhartiya@gmail.com>
2025-02-23 14:16:30 +0530

    add proper naming to paper files

--- ai_content_engine/process_paper.py
+++ ai_content_engine/process_paper.py
@@ -1,10 +1,18 @@
 import requests
 import pymupdf4llm
 import pathlib
+import re
 
 
-def download_arxiv_pdf(arxiv_url, save_path="ai_content_engine/papers/paper.pdf"):
+def extract_arxiv_id(url: str) -> str:
+    match = re.search(r"arxiv\.org/pdf/(\d+\.\d+v\d+)\.pdf", url)
+    return match.group(1) if match else None
+
+
+def download_arxiv_pdf(arxiv_url, save_path_base="ai_content_engine/papers/"):
     response = requests.get(arxiv_url)
+    paper_id = extract_arxiv_id(arxiv_url)
+    save_path = save_path_base + f"{paper_id}.pdf"
     if response.status_code == 200:
         with open(save_path, "wb") as f:
             f.write(response.content)
@@ -15,8 +23,9 @@ def download_arxiv_pdf(arxiv_url, save_path="ai_content_engine/papers/paper.pdf"
 
 def extract_markdown_from_pdf(pdf_path):
     md_text = pymupdf4llm.to_markdown(pdf_path)
-    # save markdown to file
-    pathlib.Path("ai_content_engine/papers/output.md").write_bytes(md_text.encode())
+    pdf_name = pathlib.Path(pdf_path).name
+    md_path = "ai_content_engine/papers/" + pdf_name.replace(".pdf", ".md")
+    pathlib.Path(md_path).write_bytes(md_text.encode())
     return md_text
 
 
