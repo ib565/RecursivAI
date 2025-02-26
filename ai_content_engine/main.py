0c7c470dfa9553c9d91840ecda80a80a12a85e5e
ib565 <ish.bhartiya@gmail.com>
2025-02-26 18:46:05 +0530

    update paper ID and change outline file path

--- ai_content_engine/main.py
+++ ai_content_engine/main.py
@@ -3,11 +3,11 @@ from ai_content_engine.process_paper import process_arxiv_paper
 from ai_content_engine.writer_agent import generate_blog_post
 import asyncio
 
-paper_id = "2203.02155v1"
+paper_id = "1706.03762"
 text = process_arxiv_paper(f"https://arxiv.org/pdf/{paper_id}.pdf")
 outline = generate_outline(text)
 
-with open(f"ai_content_engine/outline_{paper_id}.json", "w") as f:
+with open(f"ai_content_engine/outlines/outline_{paper_id}.json", "w") as f:
     f.write(outline.model_dump_json())
 
 blog_post = asyncio.run(generate_blog_post(outline))
