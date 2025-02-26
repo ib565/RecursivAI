41bc11a33ae4a8bb3a0626a30eac6d6196427874
ib565 <ish.bhartiya@gmail.com>
2025-02-26 18:38:49 +0530

    edit Post Model in backend

--- blog_backend/app/models/post.py
+++ blog_backend/app/models/post.py
@@ -3,15 +3,13 @@ from sqlalchemy import Column, JSON
 from datetime import datetime
 from typing import Any
 
+
 class Post(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     title: str = Field(index=True)
     slug: str = Field(index=True)
-    summary: str = Field(max_length=500)
-    content: dict[str, Any] = Field(
-        sa_column=Column(JSON), 
-        default={}
-    )
+    summary: str | None = Field(max_length=500)
+    content: dict[str, Any] = Field(sa_column=Column(JSON), default={})
     status: str = Field(default="draft")
     created_at: datetime = Field(default_factory=datetime.now)
     published_at: datetime | None = Field(default=None)
